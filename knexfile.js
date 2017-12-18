d0890d027bf04cbf3b018111ff799e15b0191e0c
Lola Brenner <amandadbrenner@gmail.com>
2017-12-18 09:16:30 -0700

    adds knexfile

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garageBin',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garageBin_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+
+};
