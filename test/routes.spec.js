45de9dca3bf6dc545882ccdb836689e2def298b9
Lola Brenner <amandadbrenner@gmail.com>
2017-12-18 10:24:29 -0700

    adds test dependencies

---
+++ test/routes.spec.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const knex = require('knex')(configuration);
+
+chai.use(chaiHttp);
