02c86e4353fdbbe4dedb0731cc5d0fde5e5cd6d0
Lola Brenner <amandadbrenner@gmail.com>
2017-12-18 10:33:47 -0700

    adds happy path test for getting garage items

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -33,3 +33,38 @@ describe('Client Routes', () => {
       });
   });
 });
+
+describe('API Routes', done => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => done())
+      .catch(error => { throw error; });
+  });
+
+  beforeEach((done) => {
+    knex.seed.run()
+      .then(() => done())
+      .catch(error => { throw error; });
+  });
+
+  describe('GET /api/v1/garageItems', () => {
+    it('should return all houses', () => {
+      return chai.request(server)
+        .get('/api/v1/garageItems')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(4);
+          response.body[0].should.have.property('name');
+          response.body[0].should.have.property('id');
+          response.body[0].name.should.equal('test-basketball');
+          response.body[0].should.have.property('reasonForLingering');
+          response.body[0].reasonForLingering.should.equal('momento from highschool games');
+          response.body[0].should.have.property('itemCleanliness');
+          response.body[0].itemCleanliness.should.equal('Dusty');
+        })
+        .catch(error => { throw error; });
+    });
+  });
+});
