f8a664ae9418f93f7ba536821609f01b1e715761
Lola Brenner <amandadbrenner@gmail.com>
2017-12-18 10:27:35 -0700

    adds client route tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,3 +7,29 @@ const configuration = require('../knexfile')[environment];
 const knex = require('knex')(configuration);
 
 chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+  it.skip('should return the homepage with text', () => {
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+      })
+      .catch(error => {
+        throw (error);
+      });
+  });
+
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
+        throw (error);
+      });
+  });
+});
